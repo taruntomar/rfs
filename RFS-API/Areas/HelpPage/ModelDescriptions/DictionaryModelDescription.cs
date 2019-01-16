88e409021bfea9403da8dfa876ef63860f495e7f
Tarun Tomar <Tarun.Tomar@Honeywell.com>
2019-01-16 13:03:00 +0530

    Front End first preview

---
+++ RFS-API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace RFS_API.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ RFS-API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RFS_API.WebApiApplication" Language="C#" %>
