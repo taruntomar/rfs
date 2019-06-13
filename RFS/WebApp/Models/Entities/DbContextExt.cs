876a17213362147c294d377028057c2b286e879f
Tarun Tomar <Tarun.Tomar@Honeywell.com>
2019-06-13 11:28:25 +0530

    some old changes

---
+++ RFS/WebApp/Models/Entities/DbContextExt.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Core.EntityClient;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace RFS.Models.Entities
+{
+    public static class DbContextExt
+    {
+        // all params are optional
+        public static void ChangeDatabase(
+            this DbContext source,
+            string initialCatalog = "",
+            string dataSource = "",
+            string userId = "",
+            string password = "",
+            bool integratedSecuity = true,
+            string configConnectionStringName = "")
+        {
+            try
+            {
+            
+                var configNameEf = string.IsNullOrEmpty(configConnectionStringName)
+                    ? source.GetType().Name
+                    : configConnectionStringName;
+
+               
+                var entityCnxStringBuilder = new EntityConnectionStringBuilder
+                    (System.Configuration.ConfigurationManager
+                        .ConnectionStrings[configNameEf].ConnectionString);
+
+                // init the sqlbuilder with the full EF connectionstring cargo
+                var sqlCnxStringBuilder = new SqlConnectionStringBuilder
+                    (entityCnxStringBuilder.ProviderConnectionString);
+
+                // only populate parameters with values if added
+                if (!string.IsNullOrEmpty(initialCatalog))
+                    sqlCnxStringBuilder.InitialCatalog = initialCatalog;
+                if (!string.IsNullOrEmpty(dataSource))
+                    sqlCnxStringBuilder.DataSource = dataSource;
+                if (!string.IsNullOrEmpty(userId))
+                    sqlCnxStringBuilder.UserID = userId;
+                if (!string.IsNullOrEmpty(password))
+                    sqlCnxStringBuilder.Password = password;
+
+              
+                sqlCnxStringBuilder.IntegratedSecurity = integratedSecuity;
+
+                source.Database.Connection.ConnectionString
+                    = sqlCnxStringBuilder.ConnectionString;
+            }
+            catch (Exception ex)
+            {
+                // set log item if required
+            }
+        }
+    }
+}
